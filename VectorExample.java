ad61d681ad586a129bbaa9676e98c7cd6f97e393
Eswar <145130369+TEswarreddy@users.noreply.github.com>
2024-04-23 09:30:02 +0530

    VectorExample.java

---
+++ VectorExample.java
@@ -0,0 +1,93 @@
+import java.util.*;
+public class ArrayListExample2
+{
+	public static void main(String[] args)
+	{
+		Vector<Integer> v1=new Vector<Integer>();
+		v1.add(1);
+		v1.add(2);
+		v1.add(3);
+		v1.add(4);
+		
+		System.out.println("Vector v1 elemnts are:"+v1);
+		
+		System.out.println("Size of the vector v1 or Total no.of elements in v1:"+v1.size());
+
+
+		Vector<Integer> v2=new Vector<Integer>();
+		v2.add(5);
+		v2.add(6);
+		v2.add(7);
+		v2.add(8);
+		System.out.println("ve2 elemnts are:"+list2);
+		
+		System.out.println("Size of the Array list2:"+list2.size());
+
+		list1.addAll(list2);
+		System.out.println("After adding tha list2 elements into list1 :");
+		Iterator itr=list1.iterator();
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+		System.out.println("Size of the Array list1:"+list1.size());
+		System.out.println("The Array list2 elements are there in Array list1:"+list1.containsAll(list2));
+		
+		list1.remove(1);
+		list1.remove(5);
+		System.out.println("After removing an objects list1 elements are:\n"+list1);
+	
+		System.out.println("Size of the Array list1:"+list1.size());
+		
+		System.out.println("The 1 elment is there(True/false):"+list1.contains(1));
+		System.out.println("The 3 elment is there(True/false):"+list1.contains(3));
+		System.out.println("The Array list2 elements are there in Array list1:"+list1.containsAll(list2));
+		
+		list1.removeAll(list2);
+		System.out.println(" after removing list2 elements from list1 elements:"+list1);
+		System.out.println("Size of the Array list1:"+list1.size());
+		
+	    list2.clear();
+		System.out.println("after clearing all elements in Array list2:"+list2);
+	    System.out.println("list2 is empty?:"+list2.isEmpty());
+	    
+	    list1.set(0,1);
+	    System .out.println("After Apple element setted into 1 specified position:"+list1);
+	    list1.set(1,1);
+	    System.out.println("Array list is:"+list1);
+	    System.out.println("Index of the 1 element is:"+list1.indexOf(1));
+	     System.out.println("Index of the 1 element from Backward or lost to first is:"+list1.lastIndexOf(1));
+	}
+}
+//OUTPUT:
+/*
+Array list 1 elemnts are:[1, 2, 3, 4]
+Size of the Array list1:4
+Array list2 elemnts are:[5, 6, 7, 8]
+Size of the Array list2:4
+After adding tha list2 elements into list1 :
+1
+2
+3
+4
+5
+6
+7
+8
+Size of the Array list1:8
+The Array list2 elements are there in Array list1:true
+After removing an objects list1 elements are:
+[1, 3, 4, 5, 6, 8]
+Size of the Array list1:6
+The 1 elment is there(True/false):true
+The 3 elment is there(True/false):true
+The Array list2 elements are there in Array list1:false
+ after removing list2 elements from list1 elements:[1, 3, 4]
+Size of the Array list1:3
+after clearing all elements in Array list2:[]
+list2 is empty?:true
+After Apple element setted into 1 specified position:[1, 3, 4]
+Array list is:[1, 1, 4]
+Index of the 1 element is:0
+Index of the 1 element from Backward or lost to first is:1
+*/
