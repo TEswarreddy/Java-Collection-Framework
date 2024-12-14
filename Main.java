4397d6aaaf18f4a02b87cfef2d43d07e4a965012
THATHIREDDY ESWAR REDDY <145130369+TEswarreddy@users.noreply.github.com>
2024-12-14 22:32:44 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,1 @@
-// You are using Java
-class Demo
-{
-    public static void main(String args[])
-    {
-        System.out.println("Hello");
-        int age =10;
-        float marks=78.80f;
-        String name="Eswar";
-        System.out.println(age);
-        System.out.println(marks);
-        System.out.println(name);
-    }
-}
+
