437aa42633b136a2be9e337a96f2136e558f971d
THATHIREDDY ESWAR REDDY <145130369+TEswarreddy@users.noreply.github.com>
2024-12-14 19:49:23 +0530

    Create Three_Sum_problem_using_HashMap.java
    Class HashMap<K,V>
    Type Parameters:
    K - the type of keys maintained by this map
    V - the type of mapped values
    
    put(K key, V value)
    Associates the specified value with the specified key in this map.
    
    containsKey(Object key)
    Returns true if this map contains a mapping for the specified key.

---
+++ Three_Sum_problem_using_HashMap.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Main 
+{
+    public static void main(String args[])
+    {
+       int a[]={1,2,3,4,2,1};
+       HashMap<Integer,Integer> map =new HashMap<>();
+       int size=a.length;
+       int target=9;
+       for(int i=0;i<size;i++)
+       {
+           for(int j=0;j<size;j++)
+           {
+               if(i==j)
+               {
+                   continue;
+               }
+               int x=target-a[i]-a[j];
+               if(map.containsKey(x) && j!=map.get(x) && i!=map.get(x))
+               {
+                   System.out.println("Indexes "+i+" "+j+" "+map.get(x));
+                   break;
+               }
+           }
+           map.put(a[i],i);
+       }
+    }
+}
+
+/* Output:
+Indexes 2 3 1
+Indexes 3 1 2
+Indexes 4 2 3
+*/
