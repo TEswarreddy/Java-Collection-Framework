05bbfe1bbf6e5c3841e21a0abfc37db3474a7276
THATHIREDDY ESWAR REDDY <145130369+TEswarreddy@users.noreply.github.com>
2024-12-17 14:32:13 +0530

    Create Anagram_using_HashMap.java

---
+++ Anagram_using_HashMap.java
@@ -0,0 +1,54 @@
+//Anagram using HashMap
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        String s="madam";
+        String t="madam";
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char x=s.charAt(i);
+            if(map.containsKey(x)){
+                map.put(x,map.get(x)+1);
+            }
+            else{
+                map.put(x,1);
+            }
+        }
+         for(int i=0;i<t.length();i++){
+            char x=t.charAt(i);
+            if(map.containsKey(x)){
+                map.put(x,map.get(x)-1);
+                
+            }
+            else{
+                System.out.println("not anagram");
+            }
+        }
+        int c=0;
+        for(int count:map.values()){
+            if(count == 0)
+            {
+                c++;
+            }
+        }
+        if(c==map.size())
+        {
+            System.out.println("Anagram");
+        }
+        
+        
+    }
+}
+/* 
+Input:
+  String s="madam";
+  String t="madam";
+Output:
+  Anagram
+
+Input:
+    String s="madam";
+    String t="madat";
+Output:
+  not anagram
+*/
